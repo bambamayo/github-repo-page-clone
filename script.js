0396c3bd2af4111f5190f843a2ff10fa8bff6b1c
bambamayo <ayobamiagunroye@gmail.com>
2020-11-23 23:52:42 +0100

    fix fetch error, use fetched data for user details

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-const user = {
-  query: `{
-    user(login: "bambamayo") {
-      avatarUrl
-      name
-      bio
-      login
-      repositories(first: 20) {
-        edges {
-          node {
-            description
-            forkCount
-            name
-            stargazerCount
-            updatedAt
-            languages(first: 1) {
-              edges {
-                node {
-                  color
-                  name
+let user_fullname = document.querySelector(".user-details__name");
+let user_username = document.querySelector(".user-details__username");
+let user_bio = document.querySelector(".user-details__info");
+let profile_image = document.querySelector(".profile_image");
+let profile_picture = document.querySelector(".profile-picture");
+
+async function getUserData() {
+  const FETCH_USER = {
+    query: `{
+      user(login: "bambamayo") {
+        avatarUrl
+        name
+        bio
+        login
+        repositories(first: 20) {
+          edges {
+            node {
+              description
+              forkCount
+              name
+              stargazerCount
+              updatedAt
+              languages(first: 1) {
+                edges {
+                  node {
+                    color
+                    name
+                  }
                 }
               }
             }
@@ -25,20 +33,32 @@ const user = {
         }
       }
     }
-  }
-  `,
-};
+    `,
+  };
 
-let body = JSON.stringify(user);
-fetch("https://developer.github.com/v4/explorer/", {
-  method: "post",
-  mode: "no-cors",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: body,
-})
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-  });
+  let body = JSON.stringify(FETCH_USER);
+
+  fetch("https://api.github.com/graphql", {
+    method: "post",
+    headers: {
+      Authorization: `Bearer 070e99cb24711e634431c8d2c20443ba459730cb`,
+    },
+    body: body,
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      user_fullname.textContent = data.data.user.name;
+      user_username.textContent = data.data.user.login;
+      user_bio.textContent = data.data.user.bio;
+      profile_image.src = data.data.user.avatarUrl;
+      profile_image.alt = `${data.data.user.name} avatar`;
+      profile_picture.src = data.data.user.avatarUrl;
+      profile_picture.alt = `${data.data.user.name} avatar`;
+      profile_image.style.visibility = "visible";
+      profile_picture.style.visibility = "visible";
+
+      console.log(data);
+    });
+}
+
+getUserData();
